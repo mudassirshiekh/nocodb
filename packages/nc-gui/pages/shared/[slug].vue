0e82822913f17138b3d78de5249ca428f62fc516
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-11-26 05:46:52 +0000

    fix(nc-gui): create new page for shared base/view custom url

---
+++ packages/nc-gui/pages/shared/[slug].vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+definePageMeta({
+  public: true,
+  requiresAuth: false,
+  hasSidebar: false,
+})
+
+const route = useRoute()
+
+useMetas()
+</script>
+
+<template>
+  <div class="h-full">This is shared page use for redirection purpose only {{ route.params.slug }}</div>
+</template>
+
+<style lang="scss" scoped></style>
